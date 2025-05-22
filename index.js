ef84ab386a9b2215a1e8ca5b37fe929fdbbe0fd3
heux16 <harshdadsena6@gmail.com>
2025-05-22 23:48:10 +0530

    2nd

--- index.js
+++ index.js
@@ -1,49 +1,58 @@
 var h = document.querySelector("h1");
-var a = document.querySelector("#in1");
+var min = document.querySelector("#in1");
+var sec = document.querySelector("#in2");
 var b= document.querySelector("#div2");
 var bt= document.querySelector("#btn1")
 var r= document.querySelector("#reset");
 var c= document.getElementsByClassName("sec");
 var rs= document.querySelector("#reset");
 var s = new Audio("src/no_surprises_doorbell.mp3")
+var timerInterval = null;
 
 document.querySelector("#sec5").addEventListener("click", function(){
-    a.value=5;
+    min.value=0;
+    sec.value=5;
 });
 document.querySelector("#sec30").addEventListener("click", function(){
-    a.value= 30;
+    min.value=0;
+    sec.value= 30;
 });
 document.querySelector("#min10").addEventListener("click", function(){
-    a.value= 600;
+    min.value=10;
+    sec.value= 0;
 });
 document.querySelector("#min15").addEventListener("click", function(){
-    a.value= 900;
+    min.value=15;
+    sec.value= 0;
 });
 bt.addEventListener("click", timer);
 rs.addEventListener("click",reset);
-var s = new Audio("src/no_surprises_doorbell.mp3")
+var s = new Audio("src/no_surprises_doorbell.mp3");
+
 
 function timer(){
-    
-    const countdown = setInterval(() => {
-    if(a.value<=0){
-        clearInterval(countdown);
+    if (timerInterval) {
+        clearInterval(timerInterval);
+    }
+    var tim= parseInt(min.value*60) + parseInt(sec.value);
+    timerInterval = setInterval(() => {
+    if(tim<=0 ){
+        clearInterval(timerInterval);
+        timerInterval = null;
         h.textContent= "times up!!"
         s.play();
     }else{
-        let x = a.value;
-        h.textContent= a.value
-        a.value--
+        var x = tim;
+        h.textContent= tim;
+        tim--
         start();
     }
     }, 1000);
-    
-    
-
 }
 
 function start(){
-    a.classList.add("invi");
+    min.classList.add("invi");
+    sec.classList.add("invi")
     bt.classList.add("invi");
     for (let i = 0; i < c.length; i++) {
         c[i].classList.add("invi");
@@ -55,17 +64,24 @@ function start(){
 
 }
 function reset(){
+    if (timerInterval) {
+        clearInterval(timerInterval);
+        timerInterval = null;
+    }
     h.textContent = "Timer";
     h.style.fontSize = ""; 
     
-    
-    a.classList.remove("invi");
+    sec.classList.remove("invi")
+    min.classList.remove("invi");
     bt.classList.remove("invi");
     for (let i = 0; i < c.length; i++) {
         c[i].classList.remove("invi");
     }
     r.classList.add("invi");
     r.classList.remove("btn");
-    a.value = "";
+    min.value = "";
+    sec.value= "";
+    tim=0;
+    x=0;
     s.pause();
 }
